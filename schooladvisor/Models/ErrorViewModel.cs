4653cdb09abee778228369814c858d205f6bece8
thomas <53865372+thomasgarbui@users.noreply.github.com>
2024-03-27 15:42:31 +0100

    togli tutto dio po

--- schooladvisor/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace schooladvisor.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
